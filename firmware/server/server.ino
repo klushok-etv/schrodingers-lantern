fb9d2729c352b24ade02973b5ad7a7f1fb942c0c
Bram <bramdenouden96@hotmail.com>
2021-07-16 16:36:59 +0200

    cleanup

--- firmware/server/server.ino
+++ firmware/server/server.ino
@@ -46,14 +46,11 @@ bool state = false;
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 
-
-
+// Define webserver on port 80
 AsyncWebServer server(80);
 
 const char* ssid = SSID_1;
 const char* password = PASSWORD_1;
-
-const char* PARAM_MESSAGE = "message";
 
 uint8_t rgb[] = {163, 0, 0}; // default color
 uint8_t intensity = 100; // only saved to be able to return this value to the web UI, color intensity calculated client side
@@ -69,9 +66,10 @@ void setup() {
   pinMode(BTN_PIN, INPUT);
   Serial.begin(115200);
 
-  // init leds
+  // Initialize leds
   FastLED.addLeds<WS2813, DATA_PIN, GRB>(leds, NUM_LEDS);
 
+  // wifi setup
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   if (WiFi.waitForConnectResult() != WL_CONNECTED) {
@@ -80,7 +78,6 @@ void setup() {
     blinkLantern(2, 500, CRGB::Red);
     return;
   }
-
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
@@ -91,113 +88,19 @@ void setup() {
     bootErrors = true;
     return;
   }
-  listDir(SPIFFS, "/", 1);
+  listDir(SPIFFS, "/", 1); // show files saved on SPIFF
   Serial.println("starting server");
 
-  // NOTE ensure the files have been uploaded to SPIFFS using the dedicated uploader!
-  // show index.html
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
-    //Send index.htm with default content type
-    request->send(SPIFFS, "/index.html");
-  });
-
-  // make css available
-  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest * request) {
-    //Send index.htm with default content type
-    request->send(SPIFFS, "/style.css");
-  });
-
-  // make js script available
-  server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest * request) {
-    //Send index.htm with default content type
-    request->send(SPIFFS, "/script.js");
-  });
-
-  // make logo available
-  server.on("/logo.svg", HTTP_GET, [](AsyncWebServerRequest * request) {
-    //Send index.htm with default content type
-    request->send(SPIFFS, "/logo.svg");
-  });
-
-  // make js script available
-  server.on("/klushok.svg", HTTP_GET, [](AsyncWebServerRequest * request) {
-    //Send index.htm with default content type
-    request->send(SPIFFS, "/klushok.svg");
-  });
-
-  // api set endpoint
-  server.on("/api/set", HTTP_GET, [](AsyncWebServerRequest * request) {
-    String message;
-    if ( request->hasParam("red") &&
-         request->hasParam("green") &&
-         request->hasParam("blue") &&
-         request->hasParam("intensity")) {
-
-      rgb[0] = request->getParam("red")->value().toInt();
-      rgb[1] = request->getParam("green")->value().toInt();
-      rgb[2] = request->getParam("blue")->value().toInt();
-      intensity = request->getParam("intensity")->value().toInt();
-
-      message = "{\"status\": \"OK\"}";
-    } else {
-      message = "{\"status\": \"ERROR\", \"message\":\"missing parameters\"}";
-    }
-    request->send(200, "application/json", message);
-    turnOn(); // set color
-  });
-
-  // api toggle endpoint
-  server.on("/api/toggle", HTTP_GET, [](AsyncWebServerRequest * request) {
-    String message;
-    if (request->hasParam("state")) {
-      Serial.println(request->getParam("state")->value().toInt());
-      Serial.println(bool(request->getParam("state")->value().toInt()));
-
-      if (request->getParam("state")->value().toInt() == 1) {
-        turnOn();
-        message = "{\"status\":\"OK\"";
-      }
-      else if (request->getParam("state")->value().toInt() == 0) {
-        turnOff();
-        message = "{\"status\":\"OK\"";
-      }
-      else {
-        message = "{\"status\":\"ERROR\", \"message\":\"Invalid parameter supplied\"";
-      }
-    } else {
-      message = "{\"status\": \"ERROR\", \"message\":\"missing parameters\"}";
-    }
-    request->send(200, "application/json", message);
-  });
-
-  // api get info endpoint
-  server.on("/api/status", HTTP_GET, [](AsyncWebServerRequest * request) {
-    AsyncResponseStream *response = request->beginResponseStream("application/json");
-    DynamicJsonDocument jsonBuffer(1024);
-    JsonArray json_rgb = jsonBuffer.createNestedArray("rgb");
-    json_rgb.add(rgb[0]);
-    json_rgb.add(rgb[1]);
-    json_rgb.add(rgb[2]);
-    jsonBuffer["intensity"] = intensity;
-    jsonBuffer["state"] = state ? "on" : "off";
-    jsonBuffer["ssid"] = WiFi.SSID();
-    jsonBuffer["RSSI"] = WiFi.RSSI();
-    jsonBuffer["localIP"] = WiFi.localIP();
-    jsonBuffer["hostName"] = WiFi.getHostname();
-
-    jsonBuffer["status"] = "OK";
-    serializeJson(jsonBuffer, *response);
-    request->send(response);
-  });
-
+  // Initialize webpages
+  setupWebPages();
   server.onNotFound(notFound);
-
   server.begin();
 
   // indicate succesfull boot
-  if(!bootErrors){
-  blinkLantern(2, 500, CRGB::Green);}
-  else{
+  if (!bootErrors) {
+    blinkLantern(2, 500, CRGB::Green);
+  }
+  else {
     Serial.println("Some boot errors occured");
     blinkLantern(2, 500, CRGB::Blue);
   }
@@ -248,12 +151,4 @@ void loop() {
   } else {
     prev_btn_state = curBtnState;
   }
-
-  //  // Turn the LED on, then pause
-  //  for ( int i = 0; i < NUM_LEDS; i++) {
-  //    leds[i] = CRGB(rgb[0], rgb[2], rgb[1]);
-  //    FastLED.show();
-  //    FastLED.delay(100);
-  //  }
-
 }
---
+++ firmware/server/webPages.ino
@@ -0,0 +1,101 @@
+void setupWebPages() {
+  // NOTE ensure the files have been uploaded to SPIFFS using the dedicated uploader!
+  // show index.html
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    //Send index.htm with default content type
+    request->send(SPIFFS, "/index.html");
+  });
+
+  // make css available
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest * request) {
+    //Send index.htm with default content type
+    request->send(SPIFFS, "/style.css");
+  });
+
+  // make js script available
+  server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest * request) {
+    //Send index.htm with default content type
+    request->send(SPIFFS, "/script.js");
+  });
+
+  // make logo available
+  server.on("/logo.svg", HTTP_GET, [](AsyncWebServerRequest * request) {
+    //Send index.htm with default content type
+    request->send(SPIFFS, "/logo.svg");
+  });
+
+  // make js script available
+  server.on("/klushok.svg", HTTP_GET, [](AsyncWebServerRequest * request) {
+    //Send index.htm with default content type
+    request->send(SPIFFS, "/klushok.svg");
+  });
+
+  // api set endpoint
+  server.on("/api/set", HTTP_GET, [](AsyncWebServerRequest * request) {
+    String message;
+    if ( request->hasParam("red") &&
+         request->hasParam("green") &&
+         request->hasParam("blue") &&
+         request->hasParam("intensity")) {
+
+      rgb[0] = request->getParam("red")->value().toInt();
+      rgb[1] = request->getParam("green")->value().toInt();
+      rgb[2] = request->getParam("blue")->value().toInt();
+      intensity = request->getParam("intensity")->value().toInt();
+
+      message = "{\"status\": \"OK\"}";
+    } else {
+      message = "{\"status\": \"ERROR\", \"message\":\"missing parameters\"}";
+    }
+    request->send(200, "application/json", message);
+    turnOn(); // set color
+  });
+
+  // api toggle endpoint
+  server.on("/api/toggle", HTTP_GET, [](AsyncWebServerRequest * request) {
+    String message;
+    if (request->hasParam("state")) {
+      Serial.println(request->getParam("state")->value().toInt());
+      Serial.println(bool(request->getParam("state")->value().toInt()));
+
+      if (request->getParam("state")->value().toInt() == 1) {
+        turnOn();
+        message = "{\"status\":\"OK\"";
+      }
+      else if (request->getParam("state")->value().toInt() == 0) {
+        turnOff();
+        message = "{\"status\":\"OK\"";
+      }
+      else {
+        message = "{\"status\":\"ERROR\", \"message\":\"Invalid parameter supplied\"";
+      }
+    } else {
+      message = "{\"status\": \"ERROR\", \"message\":\"missing parameters\"}";
+    }
+    request->send(200, "application/json", message);
+  });
+
+  // api get info endpoint
+  server.on("/api/status", HTTP_GET, [](AsyncWebServerRequest * request) {
+    AsyncResponseStream *response = request->beginResponseStream("application/json");
+    DynamicJsonDocument jsonBuffer(1024);
+    JsonArray json_rgb = jsonBuffer.createNestedArray("rgb");
+    json_rgb.add(rgb[0]);
+    json_rgb.add(rgb[1]);
+    json_rgb.add(rgb[2]);
+    jsonBuffer["intensity"] = intensity;
+    jsonBuffer["state"] = state ? "on" : "off";
+    jsonBuffer["ssid"] = WiFi.SSID();
+    jsonBuffer["RSSI"] = WiFi.RSSI();
+    jsonBuffer["localIP"] = WiFi.localIP();
+    jsonBuffer["hostName"] = WiFi.getHostname();
+
+    jsonBuffer["status"] = "OK";
+    serializeJson(jsonBuffer, *response);
+    request->send(response);
+  });
+
+
+
+
+}
